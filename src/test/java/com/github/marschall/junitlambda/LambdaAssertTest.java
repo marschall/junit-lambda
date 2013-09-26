2227fcee70c14e8f9d9c24f0f67864d0299efa7c
Philippe Marschall <philippe.marschall@gmail.com>
2013-09-26 13:06:38 -0700

    λ ftw

--- src/test/java/com/github/marschall/junitlambda/LambdaAssertTest.java
+++ src/test/java/com/github/marschall/junitlambda/LambdaAssertTest.java
@@ -1,101 +1,57 @@
 package com.github.marschall.junitlambda;
 
-import static com.github.marschall.junitlambda.LambdaAssert.assertRaises;
+import org.junit.Test;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import org.junit.Test;
+import static com.github.marschall.junitlambda.LambdaAssert.assertRaises;
 
 public class LambdaAssertTest {
   
   @Test(expected = AssertionError.class)
   public void doesNotRaiseRuntime() {
-    assertRaises(new Block() {
-      
-      public void value() {
-      }
-    }, RuntimeException.class);
+    assertRaises(() -> { }, RuntimeException.class);
   }
   
   @Test(expected = AssertionError.class)
   public void doesRaiseWrongRuntime() {
-    assertRaises(new Block() {
-      
-      @Override
-      public void value() throws Exception {
-        throw new NullPointerException();
-      }
-    }, IllegalArgumentException.class);
+    assertRaises(() -> { throw new NullPointerException();}, IllegalArgumentException.class);
   }
   
   @Test(expected = AssertionError.class)
   public void doesRaiseCheckedInsteadOfRuntime() {
-    assertRaises(new Block() {
-      
-      public void value() throws IOException {
-        throw new IOException();
-      }
-    }, RuntimeException.class);
+    assertRaises(() -> { throw new IOException();}, RuntimeException.class);
   }
 
   @Test
   public void doesRaiseRuntimeExact() {
-    assertRaises(new Block() {
-
-      public void value() {
-        throw new RuntimeException();
-      }
-    }, RuntimeException.class);
+    assertRaises(() -> { throw new RuntimeException(); }, RuntimeException.class);
   }
   
   @Test
   public void doesRaiseRuntimeSubclass() {
-    assertRaises(new Block() {
-      
-      public void value() {
-        throw new IndexOutOfBoundsException();
-      }
-    }, RuntimeException.class);
+    assertRaises(() -> { throw new IndexOutOfBoundsException();}, RuntimeException.class);
   }
   
   @Test
   public void doesRaiseCheckedExact() {
-    assertRaises(new Block() {
-      
-      public void value() throws IOException {
-        throw new IOException();
-      }
-    }, IOException.class);
+    assertRaises(() -> { throw new IOException();}, IOException.class);
   }
   
   @Test
   public void doesRaiseCheckedSubclass() {
-    assertRaises(new Block() {
-      
-      public void value() throws IOException {
-        throw new FileNotFoundException();
-      }
-    }, IOException.class);
+    assertRaises(() -> { throw new FileNotFoundException();}, IOException.class);
   }
   
   @Test
   public void assertionErrorRaised() {
-    assertRaises(new Block() {
-      
-      public void value() {
-        throw new AssertionError();
-      }
-    }, AssertionError.class);
+    assertRaises(() -> { throw new AssertionError();}, AssertionError.class);
   }
   
   @Test(expected = AssertionError.class)
   public void assertionErrorNotRaised() {
-    assertRaises(new Block() {
-      
-      public void value() {
-      }
-    }, AssertionError.class);
+    assertRaises(() -> { }, AssertionError.class);
   }
 
 }
